7b13e1d96307be4d8b6dac36cc13813b5e2ea9d4
underccity <lenich.ru@mail.ru>
2020-06-09 21:41:21 +0400

    init commit

---
+++ eve/src/main/java/underccity/eve/ManufacturingApplication.java
@@ -0,0 +1,13 @@
+package underccity.eve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManufacturingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManufacturingApplication.class, args);
+	}
+
+}
